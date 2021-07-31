a984bd5bb918e8bd6d5e5228a15f709308ef8aed
grzegorzglabpl <grzegorz.glab@glab.pl>
2021-07-31 13:50:40 +0200

    3 files added

---
+++ src/pages/Kontakt.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Kontakt = () => {
+  return (
+    <section>
+      <h1>Kontakt</h1>
+      <p>jestes na stronie kontakt</p>
+    </section>
+  );
+};
+
+export default Kontakt;
---
+++ src/pages/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import StronaGlowna from './StronaGlowna';
+import Kontakt from './Kontakt';
+
+export default {
+  StronaGlowna: StronaGlowna,
+  Kontakt: Kontakt
+};
