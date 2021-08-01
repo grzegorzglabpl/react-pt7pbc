39f7c494b95f7a674bf04e99e33f0d230a25e000
grzegorzglabpl <grzegorz.glab@glab.pl>
2021-08-01 10:31:38 +0200

    2 files modified

--- src/components/FormularzRejestracji.js
+++ src/components/FormularzRejestracji.js
@@ -3,23 +3,77 @@ import './FormularzRejestracji.css';
 
 const FormularzRejestracji = () => {
   return (
-    <form id="formularzRejestracji" method="post">
+    <form action="" id="formularzRejestracji" method="get">
+      <input
+        type="hidden"
+        name="nazwaFormularza"
+        value="formularzRejestracji"
+      />
       <h2>Formularz Rejestracji:</h2>
+
       <fieldset id="DanePersonalne">
         <legend>Dane Personalne:</legend>
         <label for="nazwisko">Podaj Nazwisko:</label>
-        <input id="nazwisko" name="nazwisko" type="text" value="" />
+        <input
+          id="nazwisko"
+          name="nazwisko"
+          type="text"
+          required
+          placeholder="Podaj Nazwisko"
+          title="Proszę podać Nazwisko"
+          minlength="3"
+          maxlength="20"
+        />
+        <p class="walidator">Niepoprawna wartość.</p>
         <label for="imie">Podaj Imię:</label>
-        <input id="imie" name="imie" type="text" value="" />
+        <input
+          id="imie"
+          name="imie"
+          type="text"
+          required
+          placeholder="Podaj Imię"
+          title="Proszę podać imię"
+          minlength="3"
+          maxlength="20"
+        />
+        <p class="walidator">Niepoprawna wartość.</p>
       </fieldset>
+
       <fieldset>
         <legend>Dane Kontaktowe:</legend>
+        <label for="telefon">Podaj numer telefonu:</label>
+        <input
+          id="telefon"
+          name="telefon"
+          type="text"
+          required
+          minlength="9"
+          maxlength="9"
+          pattern="^\d{9}$"
+          placeholder="Podaj numer telefonu w formacie: 123456789"
+        />
+        <label for="email">Podaj adres email:</label>
+        <input
+          id="email"
+          type="email"
+          required
+          placeholder="Podaj adres email"
+        />
+        <label for="stronaWWW">Adres strony WWW:</label>
+        <input
+          id="stronaWWW"
+          name="stronaWWW"
+          type="url"
+          placeholder="Poda adres strony www"
+        />
       </fieldset>
       <fieldset>
         <legend>Zgody:</legend>
       </fieldset>
       <nav>
-        <button type="submit">Rejestracja</button>
+        <button title="Zarejestruj się w systemie" type="submit">
+          &rarr; Rejestracja
+        </button>
         <input type="submit" value="Rejestracja" />
         <button type="reset">Wyczyść Formularz</button>
         <input type="reset" value="Wyczyść Formularz" />
